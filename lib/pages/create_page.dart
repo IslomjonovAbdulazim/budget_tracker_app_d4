fe21815a8fe959d224a4450f47324ba350c6395d
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-23 17:10:14 +0500

    Initial commit

---
+++ lib/pages/create_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CreatePage extends StatefulWidget {
+  const CreatePage({super.key});
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
