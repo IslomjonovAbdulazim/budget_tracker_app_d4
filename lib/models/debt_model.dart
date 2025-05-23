070196366d8ac76d14ac4853877d6dbc06dff1b0
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-23 17:34:13 +0500

    imp

--- lib/models/debt_model.dart
+++ lib/models/debt_model.dart
@@ -1,1 +1,47 @@
-class DebtModel {}
+class DebtModel {
+  // members
+  late int sum;
+  late String desc;
+  late String name;
+  late int phoneNumber;
+  late DateTime date;
+  late DateTime createdAt;
+
+  // constructor
+  DebtModel({
+    required this.sum,
+    required this.desc,
+    required this.name,
+    required this.phoneNumber,
+    required this.createdAt,
+    required this.date,
+  });
+
+  // fromJson
+  DebtModel.fromJson(Map json) {
+    sum = json["sum"];
+    desc = json["desc"];
+    name = json["name"];
+    phoneNumber = json["phoneNumber"];
+    createdAt = DateTime.parse(json["createdAt"]);
+    date = DateTime.parse(json["date"]);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
