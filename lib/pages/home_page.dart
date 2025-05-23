ca3a9f1dec8ec4177d8f6514b9021c3d8ac91c39
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-23 18:15:50 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:budget_tracker_app_d4/pages/create_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +12,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    load();
+    super.initState();
+  }
+
+  void load() async {}
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () async {
+              await Get.to(CreatePage());
+              load();
+            },
+            child: Icon(Icons.add, size: 30),
+          ),
+          SizedBox(width: 10),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
